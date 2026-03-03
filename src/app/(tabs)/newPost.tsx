f8913bd79129c40c734d83d302e804d5282570ba
c0deh0ng <hsw0323@naver.com>
2026-03-03 13:32:43 +0900

    - initial commit

---
+++ src/app/(tabs)/newPost.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function NewPostScreen() {
+  return (
+    <View>
+      <Text>NewPostScreen!</Text>
+    </View>
+  )
+}
